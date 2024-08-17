84eeadbd13c36c99aac0496f216ad233e02c2142
lemus777 <lemus777@gmail.com>
2024-08-17 19:35:59 +0200

    validating data and doing post to rest api

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,7 +1,27 @@
+import { safeParse } from 'valibot'
+import axios from 'axios';
+import { DraftProductSchema } from "../types"
+
 type ProductData = {
   [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct(data : ProductData) {
-  console.log(data)
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price
+    })
+    if(result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products`
+      const { data } = await axios.post(url, {
+        name: result.output.name,
+        price: result.output.price
+      })
+    } else {
+      throw new Error('Datos no válidos')
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
