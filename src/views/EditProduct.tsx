5e47abca974cbc48f00a677aed2cc2fdf7be2387
lemus777 <lemus777@gmail.com>
2024-08-19 17:44:45 +0200

    edit internally

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,4 +1,4 @@
-import { ActionFunctionArgs, Form, Link, redirect, useActionData } from "react-router-dom";
+import { ActionFunctionArgs, Form, Link, redirect, useActionData, useLocation } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct } from "../services/ProductService";
 
@@ -18,8 +18,10 @@ export async function action({request} : ActionFunctionArgs) {
 }
 
 export default function EditProduct() {
-
   const error = useActionData() as string
+  const { state } = useLocation()
+
+  console.log(state)
 
   return (
     <>
@@ -51,6 +53,7 @@ export default function EditProduct() {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Nombre del Producto"
             name="name"
+            defaultValue={state.product.name}
           />
         </div>
         <div className="mb-4">
@@ -64,6 +67,7 @@ export default function EditProduct() {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Precio Producto. ej. 200, 300"
             name="price"
+            defaultValue={state.product.price}
           />
         </div>
         <input
