3e54c80b20010051afab6e3b8bb57e8f63e57b24
lemus777 <lemus777@gmail.com>
2024-08-19 18:11:51 +0200

    editing by url

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,6 +1,12 @@
-import { ActionFunctionArgs, Form, Link, redirect, useActionData, useLocation } from "react-router-dom";
+import { ActionFunctionArgs, Form, Link, LoaderFunctionArgs, redirect, useActionData } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct } from "../services/ProductService";
+
+export async function loader({params} : LoaderFunctionArgs) {
+  console.log(params.id)
+
+  return {}
+}
 
 export async function action({request} : ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData())
@@ -19,9 +25,6 @@ export async function action({request} : ActionFunctionArgs) {
 
 export default function EditProduct() {
   const error = useActionData() as string
-  const { state } = useLocation()
-
-  console.log(state)
 
   return (
     <>
@@ -53,7 +56,6 @@ export default function EditProduct() {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Nombre del Producto"
             name="name"
-            defaultValue={state.product.name}
           />
         </div>
         <div className="mb-4">
@@ -67,7 +69,6 @@ export default function EditProduct() {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Precio Producto. ej. 200, 300"
             name="price"
-            defaultValue={state.product.price}
           />
         </div>
         <input
