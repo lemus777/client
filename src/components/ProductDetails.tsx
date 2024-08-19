76b0e53ba27ca7c2fa93e361eb5e2f85ad7bfecb
lemus777 <lemus777@gmail.com>
2024-08-19 13:32:20 +0200

    button for edit with Link

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -22,7 +23,10 @@ export default function ProductDetails({product} : ProductDetailsProps) {
       </td>
       <td className="p-3 text-lg text-gray-800 ">
           <div className="flex gap-2 items-center">
-            <button>Editar</button>
+            <Link
+              to={`/productos/${product.id}/editar`}
+              className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+            >Editar</Link>
           </div>
       </td>
     </tr>
