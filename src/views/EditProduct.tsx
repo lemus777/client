3bce2ad829c83496f41b271727ce6ef9346325e6
lemus777 <lemus777@gmail.com>
2024-08-20 13:09:49 +0200

    form component created

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -2,6 +2,7 @@ import { ActionFunctionArgs, Form, Link, LoaderFunctionArgs, redirect, useAction
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct, getProductById, updateProduct } from "../services/ProductService";
 import { Product } from "../types";
+import ProductForm from "../components/ProductForm";
 
 export async function loader({params} : LoaderFunctionArgs) {
   if(params.id !== undefined) {
@@ -57,34 +58,9 @@ export default function EditProduct() {
         method="POST"
       >
       
-        <div className="mb-4">
-          <label
-            className="text-gray-800"
-            htmlFor="name"
-          >Nombre Producto:</label>
-          <input 
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-            defaultValue={product.name}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="text-gray-800"
-            htmlFor="price"
-          >Precio:</label>
-          <input 
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-            defaultValue={product.price}
-          />
-        </div>
+        <ProductForm
+          product={product}
+        />
         <div className="mb-4">
           <label
             className="text-gray-800"
@@ -104,7 +80,7 @@ export default function EditProduct() {
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-          value="Registrar Producto"
+          value="Guardar cambios"
         />
       </Form>
     </>
