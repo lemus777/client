dbe634f33d9d79c5dbf00f7db9f4dd60e0eb894a
lemus777 <lemus777@gmail.com>
2024-08-20 12:07:08 +0200

    adding availability field in edit

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,6 +1,7 @@
 import { ActionFunctionArgs, Form, Link, LoaderFunctionArgs, redirect, useActionData, useLoaderData } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct, getProductById, updateProduct } from "../services/ProductService";
+import { Product } from "../types";
 
 export async function loader({params} : LoaderFunctionArgs) {
   if(params.id !== undefined) {
@@ -27,6 +28,11 @@ export async function action({request, params} : ActionFunctionArgs) {
     return redirect('/')
   }
 }
+
+const availabilityOptions = [
+  { name: 'Disponible', value: true},
+  { name: 'No Disponible', value: false}
+]
 
 export default function EditProduct() {
   const product = useLoaderData() as Product
@@ -78,6 +84,22 @@ export default function EditProduct() {
             name="price"
             defaultValue={product.price}
           />
+        </div>
+        <div className="mb-4">
+          <label
+            className="text-gray-800"
+            htmlFor="availability"
+          >Disponibilidad:</label>
+          <select 
+            id="availability"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            name="availability"
+            defaultValue={product?.availability.toString()}
+          >
+            {availabilityOptions.map(option => (
+              <option key={option.name} value={option.value.toString()}>{option.name}</option>
+            ))}
+          </select>
         </div>
         <input
           type="submit"
