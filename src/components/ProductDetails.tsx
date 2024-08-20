0a5b7d72edff3b124147aeb09f3a6fdecc8c9991
lemus777 <lemus777@gmail.com>
2024-08-20 18:54:01 +0200

    fetcher for availability added

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { ActionFunctionArgs, Form, redirect, useNavigate } from "react-router-dom"
+import { ActionFunctionArgs, Form, redirect, useFetcher, useNavigate } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
 import { deleteProduct } from "../services/ProductService"
@@ -16,6 +16,7 @@ export async function action({params} : ActionFunctionArgs) {
 
 export default function ProductDetails({product} : ProductDetailsProps) {
 
+  const fetcher = useFetcher()
   const navigate = useNavigate()
   const isAvailable = product.availability
 
@@ -28,16 +29,16 @@ export default function ProductDetails({product} : ProductDetailsProps) {
         {formatCurrency(product.price)}
       </td>
       <td className="p-3 text-lg text-gray-800">
-        <form method='POST'>
+        <fetcher.Form method='POST'>
           <button
-            type='button'
-            name='availability'
-            value={product.availability.toString()}
+            type='submit'
+            name='id'
+            value={product.id}
             className={`${isAvailable ? 'text-black' : 'text-red-600'} rounded-lg p-2 text-xs uppercase font-bold w-full border border-black-100 hover:cursor-pointer`}
           >
             {isAvailable ? 'Disponible' : 'No disponible'}
           </button>
-        </form>
+        </fetcher.Form>
       </td>
       <td className="p-3 text-lg text-gray-800 ">
           <div className="flex gap-2 items-center">
--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from 'react-router-dom'
+import { ActionFunctionArgs, Link, useLoaderData } from 'react-router-dom'
 import { getProducts } from '../services/ProductService'
 import ProductDetails from '../components/ProductDetails'
 import { Product } from '../types'
@@ -8,8 +8,9 @@ export async function loader() {
   return products
 }
 
-export async function action() {
-  console.log('Desde action de actualizar')
+export async function action({request}: ActionFunctionArgs) {
+  const data = Object.fromEntries(await request.formData())
+  console.log(data)
   return {}
 }
 
