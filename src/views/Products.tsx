a27610f33f605feb25f747550c97f0da388c03e4
lemus777 <lemus777@gmail.com>
2024-08-20 18:41:49 +0200

    form for availability created

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -8,6 +8,11 @@ export async function loader() {
   return products
 }
 
+export async function action() {
+  console.log('Desde action de actualizar')
+  return {}
+}
+
 export default function Products() {
 
   const products = useLoaderData() as Product[]
